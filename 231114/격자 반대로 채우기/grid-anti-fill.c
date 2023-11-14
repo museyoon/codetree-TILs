ee9987c032dafe079e866e50a87d48d72dfe444f
museyoon <105194123+museyoon@users.noreply.github.com>
2023-11-14 17:29:49 +0900

    Title: 격자 반대로 채우기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231114/격자 반대로 채우기/grid-anti-fill.c
+++ 231114/격자 반대로 채우기/grid-anti-fill.c
@@ -1,29 +1,34 @@
 #include <stdio.h>
 
-int main() {
-    int n;
-    scanf("%d", &n);
-    int arr[10][10];
-    int num = 1;
-    int i, j;
-
-    for (i = n - 1; i >= 0; i--){
-        if (i % 2 == 0){
-            for (j = n-1; j >= 0; j--)
-                arr[j][i] = num++;
+void fillMatrix(int n, int matrix[n][n]) {
+    int value = 1;
+    for(int j = n - 1; j >= 0; j--) {
+        if(j % 2 == 0) {
+            for(int i = n - 1; i >= 0; i--) {
+                matrix[i][j] = value++;
+            }
+        } else {
+            for(int i = 0; i < n; i++) {
+                matrix[i][j] = value++;
+            }
         }
-        else{
-            for (j = 0; j < n; j++)
-                arr[j][i] = num++;
-        }
-    } 
+    }
+}
 
-    for (i = 0; i < n; i++){
-        for (j = 0; j < n; j++){
-            printf("%d ", arr[i][j]);
+void printMatrix(int n, int matrix[n][n]) {
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            printf("%d ", matrix[i][j]);
         }
         printf("\n");
     }
+}
 
+int main() {
+    int n;
+    scanf("%d", &n);
+    int matrix[n][n];
+    fillMatrix(n, matrix);
+    printMatrix(n, matrix);
     return 0;
 }
