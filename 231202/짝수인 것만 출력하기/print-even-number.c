4ec4c2f438c4ed16a16d02befeed9bacbc545143
sooooo <105194123+museyoon@users.noreply.github.com>
2023-12-02 22:35:57 +0900

    Title: 짝수인 것만 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231202/짝수인 것만 출력하기/print-even-number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n, arr[100];
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for(int i = 0; i < n; i++) {
+        if(arr[i] % 2 ==0) {
+            printf("%d", arr[i]);
+        }
+    }
+    return 0;
+}
